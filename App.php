d7f87f97cc67523c50b83c0939502d7080ddd990
Michael Eckebrecht <direkt@route-media.info>
2020-11-21 12:03:23 +0100

    Update and rename app.php to App.php

--- App.php
+++ App.php
@@ -4,7 +4,7 @@ namespace Cimply {
         public $error = false;
         private $app = null;
         function __construct($project = null) {
-			$this->app = (new Work(['cimply\\']))->app($project)->run();
+		$this->app = (new Work(['src\\']))->app($project)->run();
         }
         public function run():App\Run {
             return $this->app;
